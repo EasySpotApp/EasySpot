568d7444e4c2e7b7fc61296b4d67d90783b9455e
GGORG <GGORG0@protonmail.com>
2025-10-07 22:47:45 +0200

    enabled release optimizations

--- app/src/main/java/xyz/ggorg/easyspot/ui/main/MainScaffold.kt
+++ app/src/main/java/xyz/ggorg/easyspot/ui/main/MainScaffold.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import xyz.ggorg.easyspot.R
 
 @OptIn(ExperimentalMaterial3Api::class)
