bd926ddba94c22bf6275c46106b63aea5b10b6a2
GGORG <GGORG0@protonmail.com>
2025-10-12 18:03:58 +0200

    bumped version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "xyz.ggorg.easyspot"
         minSdk = 31
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
     }
 
     buildTypes {
